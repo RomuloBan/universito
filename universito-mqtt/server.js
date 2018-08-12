5c2b5dfa6855beeb59bb52eb71a18dc869f2609f
RomuloBanares <romulo.banares@kairosds.com>
2018-08-12 12:18:06 +0200

    save agent information and publish

--- universito-mqtt/server.js
+++ universito-mqtt/server.js
@@ -4,7 +4,8 @@ const debug = require('debug')('universito:mqtt')
 const mosca = require('mosca')
 const redis = require('redis')
 const chalk = require('chalk')
-const db    = require('universito-db')
+const db = require('universito-db')
+const { parsePayload } = require('./utils')
 
 const backend = {
   type: 'redis',
@@ -27,19 +28,63 @@ const config = {
 }
 
 const server = new mosca.Server(settings)
+const clients = new Map()
 
 let Agent, Metric
 
 server.on('clientConnected', client => {
   debug(`Client Connected: ${client.id}`)
+  clients.set(client.id, null)
 })
 
 server.on('clientDisconnected', client => {
   debug(`Client Disconnected: ${client.id}`)
 })
 
-server.on('published', (packet, client) => {
+server.on('published', async (packet, client) => {
   debug(`Received: ${packet.topic}`)
+
+  switch (packet.topic) {
+    case 'agent/connected':
+    case 'agent/disconnected':
+      debug(`Payload: ${packet.payload}`)
+      break
+    case 'agent/message':
+      debug(`Payload: ${packet.payload}`)
+
+      const payload = parsePayload(packet.payload)
+
+      if (payload) {
+        payload.agent.connected = true
+
+        let agent
+        try {
+          agent = await Agent.createOrUpdate(payload.agent)
+        } catch (err) {
+          return handleError(err)
+        }
+
+        debug(`Agent ${agent.uuid} saved`)
+
+        // Notify Agent is Connected
+        if (!clients.get(client.id)) {
+          clients.set(client.id, agent)
+          server.publish({
+            topic: 'agent/connected',
+            payload: JSON.stringify({
+              agent: {
+                uuid: agent.uuid,
+                name: agent.name,
+                hostname: agent.hostname,
+                pid: agent.pid,
+                connected: agent.connected
+              }
+            })
+          })
+        }
+      }
+      break
+  }
   debug(`Payload: ${packet.payload}`)
 })
 
@@ -56,6 +101,11 @@ function handleFatalError (err) {
   console.error(`${chalk.red('[fatal error]')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
+}
+
+function handleError (err) {
+  console.error(`${chalk.red('[error]')} ${err.message}`)
+  console.error(err.stack)
 }
 
 process.on('uncaughtException', handleFatalError)
---
+++ universito-mqtt/utils.js
@@ -0,0 +1,20 @@
+'use strict'
+
+function parsePayload (payload) {
+  if (payload instanceof Buffer) {
+    payload = payload.toString('utf8')
+  }
+
+  try {
+    payload = JSON.parse(payload)
+  } catch (err) {
+    payload = {}
+  }
+  return payload
+}
+
+module.exports = () => {
+  return {
+    parsePayload
+  }
+}
