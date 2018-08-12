c816896ed1bafa8a357dcdd6a3e3fc3c126694db
RomuloBanares <romulo.banares@kairosds.com>
2018-08-12 14:34:01 +0200

    setInterval implementation

--- universito-agent/index.js
+++ universito-agent/index.js
@@ -3,7 +3,34 @@
 const EventEmitter = require('events')
 
 class UniversitoAgent extends EventEmitter {
+  constructor (opts) {
+    super()
+    this.INTERVAL_DEFAULT = 2000
+    this._options = opts
+    this._started = false
+    this._timer = null
+    this._interval = opts ? opts.interval : this.INTERVAL_DEFAULT
+  }
 
+  connect () {
+    if (!this._started) {
+      this._started = true
+
+      this.emit('connected')
+
+      this._timer = setInterval(() => {
+        this.emit('agent/message', 'this is a message')
+      }, this._interval)
+    }
+  }
+
+  disconnect () {
+    if (this._started) {
+      clearInterval(this._timer)
+      this._started = false
+      this.emit('disconnected')
+    }
+  }
 }
 
 module.exports = UniversitoAgent
