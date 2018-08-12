a2be5fe882ee18098ffe65def58e4cbb4cdeeb89
RomuloBanares <romulo.banares@kairosds.com>
2018-08-12 22:00:17 +0200

    api routes tests

--- universito-api/tests/api-tests.js
+++ universito-api/tests/api-tests.js
@@ -2,7 +2,39 @@
 
 const test = require('ava')
 const request = require('supertest')
-const server = require('../server')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
+const agentFixtures = require('./fixtures/agent')
+
+let sandbox = null
+let server = null
+let dbStub = null
+let AgentStub = {}
+let MetricStub = {}
+
+test.beforeEach(async () => {
+  sandbox = sinon.createSandbox()
+  dbStub = sinon.stub()
+  dbStub.returns(Promise.resolve({
+    Agent: AgentStub,
+    Metric: MetricStub
+  }))
+
+  AgentStub.findConnected = sandbox.stub()
+  AgentStub.findConnected.returns(Promise.resolve(agentFixtures.connected))
+
+  const api = proxyquire('../api', {
+    'universito-db': dbStub
+  })
+
+  server = proxyquire('../server', {
+    './api': api
+  })
+})
+
+test.afterEach(() => {
+  sandbox && sandbox.restore()
+})
 
 test.serial.cb('/api/agents', t => {
   request(server)
@@ -11,8 +43,18 @@ test.serial.cb('/api/agents', t => {
     .expect('Content-Type', /json/)
     .end((err, res) => {
       t.falsy(err, 'should not return ab¡n error')
-      let body = res.body
-      t.deepEqual(body, {}, 'response body should be the expected')
+      let body = JSON.stringify(res.body)
+      let expected = JSON.stringify(agentFixtures.connected)
+      t.deepEqual(body, expected, 'response body should be the expected')
       t.end()
     })
 })
+
+test.serial.todo('/api/agents/:uuid')
+test.serial.todo('/api/agents/:uuid - not found')
+
+test.serial.todo('/api/metrics/:uuid')
+test.serial.todo('/api/metrics/:uuid - not found')
+
+test.serial.todo('/api/metrics/:uuid/:type')
+test.serial.todo('/api/metrics/:uuid/:type - not found')
