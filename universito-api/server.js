a671ff0be2d43374e23217c20c9b303c8b799567
RomuloBanares <romulo.banares@kairosds.com>
2018-08-12 19:46:51 +0200

    express error handler

--- universito-api/server.js
+++ universito-api/server.js
@@ -3,6 +3,7 @@
 const http = require('http')
 const express = require('express')
 const chalk = require('chalk')
+const debug = require('debug')
 const api = require('./api')
 
 const port = process.env.PORT || 3000
@@ -10,6 +11,26 @@ const app = express()
 const server = http.createServer(app)
 
 app.use('/api', api)
+
+// Express Error Handler
+app.use((err, req, res, next) => {
+  debug(`${chalk.red('[error]')} ${err.message}`)
+
+  if (err.message.match(/not found/)) {
+    return res.status(404).send({ error: err.message })
+  }
+
+  res.status(500).send({ error: err.message })
+})
+
+function handleFatalError (err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
 
 server.listen(port, () => {
   console.log(`${chalk.green('[universito-api]')} server listening on port ${port}`)
