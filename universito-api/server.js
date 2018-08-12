7b09be46ea9154655d3062aa42ab01c7b2c328a1
RomuloBanares <romulo.banares@kairosds.com>
2018-08-12 20:09:46 +0200

    create api tests

--- universito-api/server.js
+++ universito-api/server.js
@@ -29,9 +29,13 @@ function handleFatalError (err) {
   process.exit(1)
 }
 
-process.on('uncaughtException', handleFatalError)
-process.on('unhandledRejection', handleFatalError)
+if (!module.parent) {
+  process.on('uncaughtException', handleFatalError)
+  process.on('unhandledRejection', handleFatalError)
 
-server.listen(port, () => {
-  console.log(`${chalk.green('[universito-api]')} server listening on port ${port}`)
-})
+  server.listen(port, () => {
+    console.log(`${chalk.green('[universito-api]')} server listening on port ${port}`)
+  })
+}
+
+module.exports = server
